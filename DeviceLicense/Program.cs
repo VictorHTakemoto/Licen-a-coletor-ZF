fe0ce3f3635608f86d8321fdd9fc829fb3384389
Victor Takemoto <victor_takemoto@outlook.com>
2023-05-04 09:53:29 -0300

    Considerações finais, API WEB finalizada, pronta para deploy

--- DeviceLicense/Program.cs
+++ DeviceLicense/Program.cs
@@ -1,19 +1,60 @@
 using DeviceLicense.Model;
-using DeviceLicense.Service;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.EntityFrameworkCore;
-using Topshelf;
 
-HostFactory.Run(p =>
-            {
-                p.Service<WindowsService>(s =>
-                {
-                    s.ConstructUsing(st => new WindowsService());
-                    s.WhenStarted(st => st.start(args));
-                    s.WhenStopped(st => st.stop());
-                });
-                p.RunAsLocalSystem();
-                p.SetDescription("API Licença Coletor de Dados");
-                p.SetDisplayName("API Licença");
-                p.SetServiceName("Licença Coletor");
-            });
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+//Conexao com o Banco de dados
+var connectionString = builder.Configuration.GetConnectionString("Producao");
+builder.Services.AddDbContext<DeviceDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Define os dominios na lista de permissao do CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "AllowAnyOrigin",
+        builder =>
+        {
+            builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+        });
+});
+
+//Define o IP da maquina como como bind da API e portas
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(5224);
+    options.ListenAnyIP(7019, ListenOptions => ListenOptions.UseHttps());
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+    using var scope = app.Services.CreateScope();
+    //Deploy do Banco com add-migration
+    var db = scope.ServiceProvider.GetRequiredService<DeviceDbContext>();
+    db.Database.Migrate();
+
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("AllowAnyOrigin");
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
